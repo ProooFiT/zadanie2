eb609340804114550afafd31aca74fd6314869e8
PC <seweryn.contact@gmail.com>
2024-11-16 20:12:22 +0100

    main Doxy

--- main.cpp
+++ main.cpp
@@ -1,13 +1,34 @@
+/**
+ * @file main.cpp
+ * @brief Program zarządzający drzewem binarnym przy użyciu klasy BST oraz FileHandler.
+ * 
+ * Program pozwala na dodawanie, usuwanie elementów, wyszukiwanie ścieżki do elementu,
+ * wyświetlanie drzewa oraz zapis i odczyt drzewa z plików tekstowych i binarnych.
+ */
+
 #include "BST.h"
 #include "FileHandler.h"
 #include <iostream>
 
+/**
+ * @brief Funkcja główna programu.
+ * 
+ * Wyświetla menu z opcjami zarządzania drzewem binarnym:
+ * - Dodawanie elementów
+ * - Usuwanie elementów
+ * - Wyszukiwanie ścieżki do elementu
+ * - Wyświetlanie drzewa w różnych porządkach (preorder, inorder, postorder)
+ * - Zapisywanie i odczytywanie drzewa z plików tekstowych i binarnych
+ * 
+ * @return Zwraca 0 po zakończeniu działania programu.
+ */
 int main() {
     BST tree;
     int choice, value;
     std::string filename;
 
     while (true) {
+        // Wyświetlenie menu
         std::cout << "Menu:\n";
         std::cout << "1. Dodaj element\n";
         std::cout << "2. Usun element\n";
@@ -23,21 +44,36 @@ int main() {
         std::cin >> choice;
 
         switch (choice) {
-            case 1:
+            case 1: {
+                /** 
+                 * @brief Dodawanie elementu do drzewa.
+                 */
                 std::cout << "Podaj wartosc: ";
                 std::cin >> value;
                 tree.add(value);
                 break;
-            case 2:
+            }
+            case 2: {
+                /** 
+                 * @brief Usuwanie elementu z drzewa.
+                 */
                 std::cout << "Podaj wartosc do usuniecia: ";
                 std::cin >> value;
                 tree.remove(value);
                 break;
-            case 3:
+            }
+            case 3: {
+                /** 
+                 * @brief Usunięcie całego drzewa.
+                 */
                 tree.clear();
                 std::cout << "Drzewo usuniete.\n";
                 break;
-            case 4:
+            }
+            case 4: {
+                /** 
+                 * @brief Wyszukiwanie ścieżki do elementu w drzewie.
+                 */
                 std::cout << "Podaj wartosc do znalezienia: ";
                 std::cin >> value;
                 std::vector<int> path;
@@ -49,34 +85,61 @@ int main() {
                     std::cout << "Element nie znaleziony.\n";
                 }
                 break;
-            case 5:
+            }
+            case 5: {
+                /** 
+                 * @brief Wyświetlanie drzewa w wybranym porządku.
+                 */
                 std::cout << "Wybierz porzadek (1-preorder, 2-inorder, 3-postorder): ";
                 std::cin >> value;
                 tree.display(value);
                 break;
-            case 6:
+            }
+            case 6: {
+                /** 
+                 * @brief Zapis drzewa do pliku tekstowego.
+                 */
                 std::cout << "Podaj nazwe pliku: ";
                 std::cin >> filename;
                 tree.saveToTextFile(filename);
                 break;
-            case 7:
+            }
+            case 7: {
+                /** 
+                 * @brief Zapis drzewa do pliku binarnego.
+                 */
                 std::cout << "Podaj nazwe pliku: ";
                 std::cin >> filename;
                 FileHandler::saveBinary(filename, tree);
                 break;
-            case 8:
+            }
+            case 8: {
+                /** 
+                 * @brief Wczytanie drzewa z pliku binarnego.
+                 */
                 std::cout << "Podaj nazwe pliku: ";
                 std::cin >> filename;
                 FileHandler::loadBinary(filename, tree);
                 break;
-            case 9:
+            }
+            case 9: {
+                /** 
+                 * @brief Wczytanie drzewa z pliku tekstowego.
+                 */
                 std::cout << "Podaj nazwe pliku: ";
                 std::cin >> filename;
                 FileHandler::loadFromTextFile(filename, tree);
                 break;
+            }
             case 0:
+                /**
+                 * @brief Wyjście z programu.
+                 */
                 return 0;
             default:
+                /**
+                 * @brief Obsługa nieprawidłowej opcji menu.
+                 */
                 std::cout << "Nieprawidlowa opcja!\n";
                 break;
         }
