2a0d5fa180497b93f8e822c3ca7b9efc9f41b3df
PC <seweryn.contact@gmail.com>
2024-11-16 20:02:19 +0100

    FileHandler h Doxy

--- FILEHANDLER.h
+++ FILEHANDLER.h
@@ -4,11 +4,42 @@
 #include "BST.h"
 #include <string>
 
+/**
+ * @class FileHandler
+ * @brief Klasa odpowiedzialna za operacje na plikach dla drzewa binarnego (BST).
+ * 
+ * Klasa `FileHandler` zawiera metody do zapisu i odczytu drzewa binarnego 
+ * zarówno z plików binarnych, jak i tekstowych.
+ */
 class FileHandler {
 public:
+
+    /**
+     * @brief Zapisuje drzewo binarne do pliku binarnego.
+     * 
+     * @param filename Nazwa pliku, do którego zapisane zostanie drzewo.
+     * @param tree Obiekt drzewa binarnego (BST), który ma być zapisany.
+     */
     static void saveBinary(const std::string& filename, const BST& tree);
+
+    /**
+     * @brief Odczytuje drzewo binarne z pliku binarnego.
+     * 
+     * @param filename Nazwa pliku, z którego drzewo zostanie odczytane.
+     * @param tree Obiekt drzewa binarnego (BST), do którego wczytane zostaną dane.
+     */
     static void loadBinary(const std::string& filename, BST& tree);
+
+    /**
+     * @brief Wczytuje drzewo binarne z pliku tekstowego.
+     * 
+     * @param filename Nazwa pliku tekstowego, z którego wczytywane będą dane.
+     * @param tree Obiekt drzewa binarnego (BST), do którego wczytane zostaną wartości.
+     * 
+     * @details Funkcja otwiera plik tekstowy i odczytuje z niego kolejne liczby całkowite,
+     *          które następnie są dodawane do drzewa binarnego.
+     */
     static void loadFromTextFile(const std::string& filename, BST& tree);
 };
 
-#endif
+#endif // FILEHANDLER_H
